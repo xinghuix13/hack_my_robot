4181ee4e6cca11b206fed449dd74b612a1dbc34d
Samuel <samuel.prieto@nyu.edu>
2022-10-13 15:12:53 +0400

    Updated

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
-d = generate_distutils_setup(
-    packages=['hack_my_robot'],
-    package_dir={'': 'src'}
-)
-setup(**d)
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['smart_data_collection'],
+    package_dir={'': 'src'})
+
+setup(**setup_args)
